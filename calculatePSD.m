2dabdd2511f93d94424845ef4cd8d8fded86e042
Zach <ipzach@uw.edu>
2022-03-07 15:48:04 -0800

    Fix PSD dimensions

--- calculatePSD.m
+++ calculatePSD.m
@@ -1,10 +1,10 @@
 function [spec, f_crop] = calculatePSD(spikes,ephys, fs)
-spec = NaN(501,length(spikes));
+spec = NaN(length(spikes),501);
 for i = 1:length(spikes)
     idx = round(spikes(i) * fs);
     if idx > 1000 && idx < (length(ephys)-1000)
         [temp_spec,f] = periodogram(ephys(idx-1000:idx+1000),hamming(2001),2001,1000,'power');
-        spec(1:501,i) = temp_spec(1:501);
+        spec(i,1:501) = temp_spec(1:501);
     end
 end
 f_crop = f(1:501);
